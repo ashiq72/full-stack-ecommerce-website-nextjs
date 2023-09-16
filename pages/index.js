786c00b687ef43fa881590353d77f14d28c98b2d
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-09-16 10:21:38 +0600

    Home page heading and paragrap added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 // import { Inter } from "next/font/google";
 
 import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,37 @@ export default function Home() {
   return (
     <div>
       <HeroBanner />
+      <Wrapper>
+        {/* heading and paragaph start */}
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Cushioning for Your Miles
+          </div>
+          <div className="text-md md:text-xl">
+            A lightweight Nike ZoomX midsole is combined with increased stack
+            heights to help provide cushioning during extended stretches of
+            running.
+          </div>
+        </div>
+        {/* heading and paragaph end */}
+
+        {/* products grid start */}
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product} />
+          ))} */}
+        {/* <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard /> */}
+        {/* </div> */}
+        {/* products grid end */}
+      </Wrapper>
     </div>
   );
 }
